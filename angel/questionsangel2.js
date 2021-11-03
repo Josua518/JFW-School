9697ec18997105969fc8f1d5406b5d6ce4560797
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-11-03 07:41:15 +0700

    Add files via upload

---
+++ angel/questionsangel2.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1,
+  question: "Hasil perkalian dari 100 × 50 adalah ...",
+  answer: "5.000",
+  options: [
+    "3.000",
+    "4.000",
+    "5.000",
+    "6.000"
+  ]
+},
+  {
+    numb: 2,
+    question: "Hasil perkalian dari 124 × 47 adalah ...",
+    answer: "5.828",
+    options: [
+      "5.728",
+      "5.828",
+      "5.928",
+      "6.028"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Hasil perkalian dari 387 × 74 adalah ...",
+    answer: "28.638",
+    options: [
+      "28.238",
+      "28.338",
+      "28.538",
+      "28.638"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Hasil perkalian dari 1.379 × 21 adalah ...",
+    answer: "28.959",
+    options: [
+      "28.859",
+      "28.959",
+      "29.059",
+      "29.159"
+    ]
+  },
+  {
+    numb: 5,
+    question: "120 ÷ 20 =",
+    answer: "6",
+    options: [
+      "4",
+      "5",
+      "6",
+      "7"
+    ]
+  },
+  {
+    numb: 6,
+    question: "210 ÷ 10 =",
+    answer: "21",
+    options: [
+      "19",
+      "20",
+      "21",
+      "22"
+    ]
+  },
+  {
+    numb: 7,
+    question: "350 ÷ 50 = ",
+    answer: "7",
+    options: [
+      "5",
+      "6",
+      "7",
+      "8"
+    ]
+  },
+  {
+    numb: 8,
+    question: "570 ÷ 2 =",
+    answer: "285",
+    options: [
+      "280",
+      "282",
+      "284",
+      "285"
+    ]
+  },
+  {
+    numb: 9,
+    question: "690 ÷ 30",
+    answer: "23",
+    options: [
+      "23",
+      "24",
+      "25",
+      "30"
+    ]
+  },
+  {
+    numb: 10,
+    question: "1780 ÷ 20 =",
+    answer: "890",
+    options: [
+      "870",
+      "880",
+      "890",
+      "900"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
